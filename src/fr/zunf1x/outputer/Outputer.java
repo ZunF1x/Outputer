f41daa73ef9e4fb9c98a1e2a39ea1ceb3fc1fd43
ZunF1x <nathanbozon97@gmail.com>
2021-01-21 19:51:20 +0100

    The project

---
+++ src/fr/zunf1x/outputer/Outputer.java
@@ -0,0 +1,22 @@
+package fr.zunf1x.outputer;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Outputer {
+	
+	private static JFrame frame;
+	private static JPanel panel;
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	public static JFrame getFrame() {
+		return frame;
+	}
+	
+	public static JPanel getPanel() {
+		return panel;
+	}
+}
